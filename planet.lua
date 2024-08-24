0e6016ac1acc6187d6897f7089acddd2317e35e3
Michael Schneider <michaews@uci.edu>
2024-08-24 11:14:57 -0700

    Modified planet to be able to move in 2 dimensions.

--- planet.lua
+++ planet.lua
@@ -4,15 +4,26 @@ require 'circle'
 function makePlanet(x, y, r, image, gravR, gravImg)
     local planet = {}
     planet.circle = makeCircle(x, y, r, image)
-    planet.speed = 0
+    planet.speed = 10
     planet.gravField = makeCircle(x, y, gravR, gravImg)
+    planet.dx = 0
+    planet.dy = 0
 
-    function planet:update()
-        if planet.circle.y - planet.circle.r + planet.speed < 0 or planet.circle.y + planet.circle.r + planet.speed > WINDOW_HEIGHT then
-            planet.speed = 0
-        end
-        planet.gravField.y = planet.gravField.y + planet.speed
-        planet.circle.y = planet.circle.y + planet.speed
+    function planet:update(dt)
+        planet.circle.x = planet.circle.x + planet.dx
+        planet.gravField.x = planet.gravField.x + planet.dx
+        planet.circle.y = planet.circle.y + planet.dy
+        planet.gravField.y = planet.gravField.y + planet.dy
+    end
+
+    function planet:stop()
+        planet.dx = 0
+        planet.dy = 0
+    end
+
+    function planet:setDir(dx, dy)
+        dx, dy = getDir(0, 0, dx, dy)
+        planet.dx, planet.dy = setSpeed(dx, dy, planet.speed)
     end
 
     function planet:draw()
