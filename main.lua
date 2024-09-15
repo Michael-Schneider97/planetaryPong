3ece4d6eca8d78873b461b359482b40263ef6e3f
Michael Schneider <michaews@uci.edu>
2024-09-15 15:32:52 -0700

    Rearranged functions to improve readability.

--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
+-- Important globals
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 mobile = false
+local FORCE_MULTIPLIER = 10400
+constForce = 0.3
 
+-- libs etc
 require 'devMenu'
 require 'planet'
 require 'ball'
@@ -21,9 +25,7 @@ distance between ray and planet center sets goal speed or if planet should blow 
 planet gravity releases ball at an angle dependent on distance between ray ans planet
 ]]
 
-
-local FORCE_MULTIPLIER = 10400
-constForce = 0.3
+-- love callbacks
 
 function love.update(dt)
     gameState = {play = 'play', pause = 'pause', menu = 'menu'}
@@ -32,13 +34,77 @@ function love.update(dt)
     -- assignment every update cycle is bad but shouldnt impact performance enough to matter
     if currentState == gameState.pause then
         devMenu.show = false
-        Slab.Update(dt)
+        updateUi(dt)
 end
 
     if currentState == gameState.play then
         devMenu.show = true
         updateGravSim()
     end
+end
+
+function love.load()
+    math.randomseed(os.time())
+    Slab.Initialize()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = true,
+        resizable = false,
+        vsync = true
+    })
+  
+    -- phone setup
+    local os = love.system.getOS() 
+    if os == "Android" or os == "iOS" then
+	    mobile = true
+	end
+    
+    setupObjects()
+    love.graphics.setFont(titleFont)
+end
+
+function love.draw()
+    rs.push()
+    local old_x, old_y, old_w, old_h = love.graphics.getScissor()
+    love.graphics.setScissor(rs.get_game_zone())
+    
+    -- start drawing 
+    local titleText = "GRAVITY PONG"
+    love.graphics.clear(.1, .1, .1)
+
+    love.graphics.draw(bkg, 0, 0, 0, WINDOW_WIDTH / bkg:getWidth(), WINDOW_HEIGHT / bkg:getHeight())
+    love.graphics.printf(titleText, 0, WINDOW_HEIGHT * 0.15, WINDOW_WIDTH, 'center') 
+    planet1.draw()   
+    planet2.draw()  
+    ball.draw()  
+    scoreboard:draw()
+    
+    devMenu:draw()
+	Slab.Draw() 
+    
+    -- stop drawing
+    love.graphics.setScissor(old_x, old_y, old_w, old_h)
+	rs.pop()
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.mousepressed(_x, _y)
+	_x, _y = rs.to_game(_x, _y)
+	devMenu:handleClicks(_x, _y)
+end
+
+-- delegated functions
+
+function updateUi(dt)
+    Slab.Update(dt)
+
+    Slab.BeginWindow('MyFirstWindow', {Title = "My First Window"})
+    Slab.Text("Hello World")
+    Slab.EndWindow()
 end
 
 -- update stuff
@@ -68,54 +134,6 @@ function updateGravSim()
     devMenu.update()
 end
 
-function love.load()
-    math.randomseed(os.time())
-    Slab.Initialize()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = true,
-        resizable = false,
-        vsync = true
-    })
-  
-    -- phone setup
-    local os = love.system.getOS() 
-    if os == "Android" or os == "iOS" then
-	    mobile = true
-	end
-    
-    setupObjects()
-    love.graphics.setFont(titleFont)
-end
-
-function love.draw()
-    rs.push()
-    local old_x, old_y, old_w, old_h = love.graphics.getScissor()
-    love.graphics.setScissor(rs.get_game_zone())
-    
-    -- start drawing
-    local titleText = "GRAVITY PONG"
-    love.graphics.clear(.1, .1, .1)
-
-    love.graphics.draw(bkg, 0, 0, 0, WINDOW_WIDTH / bkg:getWidth(), WINDOW_HEIGHT / bkg:getHeight())
-    love.graphics.printf(titleText, 0, WINDOW_HEIGHT * 0.15, WINDOW_WIDTH, 'center') 
-    planet1.draw()   
-    planet2.draw()  
-    ball.draw()  
-    scoreboard:draw()
-    
-    devMenu:draw()
-    
-    -- stop drawing
-    love.graphics.setScissor(old_x, old_y, old_w, old_h)
-	rs.pop()
-end
-
-function love.keypressed(key)
-    if key == 'escape' then
-        love.event.quit()
-    end
-end
-
 -- making these global isnt the best but I'm keeping them all in this one function to make keeping track easier
 function setupObjects()
     local SIZE_SAFE_ZONE = WINDOW_WIDTH / 80
@@ -143,141 +161,6 @@ function setupObjects()
     devMenu:addVariable(getBallSpeed, "Ball Speed: ", true, setBallSpeed)
     devMenu:addVariable(getBallGoalSpd, "Ball Goal Speed: ", true, setBallGoalSpd)
     devMenu:addVariable(getGravCoef, "Gravity Coef: ", true, setGravCoef)
-end
-
--- returns the force of gravity as a vector dx / dy format
--- remember that order matters, circle1 is your planet, circle2 is your ball
-function getGravForce(circle1, circle2, forceMultiplier)
-    local BALL_MASS = 1
-    local DIST = distance(circle1.x, circle1.y, circle2.x, circle2.y)
-    local deltaX, deltaY = getDir(circle1.x, circle1.y, circle2.x, circle2.y)
-    local force = calcForce(DIST) -- reduction
-    --(forceMultiplier * BALL_MASS) / ( (DIST + circle1.r + circle2.r) * (DIST) )  -- this idea was to control gravity as a matter of surface distance
-    return deltaX * force, deltaY * force
-end
-
-function calcForce(x)
-    return constForce
-end
-
--- finds the point where a pair of lines intersect
-function getIntersect(line1y1, line1y2, line2y1, line2y2)
-	local line1Slope = getSlope(0, line1y1, WINDOW_WIDTH, line1y2)
-	local line2Slope = getSlope(0, line2y1, WINDOW_WIDTH, line2y2)
-	local x = ( line2y1 - line1y1 ) / (line1Slope - line2Slope)
-	local y = line1Slope * x + line1y1
-	-- we should handle the case where there is no intersect 
-	return x, y
-end
-
--- we conditionally set the speed of the ball and the exit angle based on distance between ball and planet ray cast:
-
-
-
--- gets the y intercepts at the edges of the window for a
--- moving object
--- im not actually 100% sure this works, mathematically speaking
-function getYIntercepts(x, y, deltaX, deltaY)
-	local yInter = y - ((deltaY / deltaX) * x) 
-	return yInter, (deltaY / deltaX) * WINDOW_WIDTH + yInter
-end
-
--- returns the slope of a line
-function getSlope(x1, y1, x2, y2)
-	if x1 == x2 then
-		return math.huge
-	end 
-	
-	return (y2 - y1) / (x2 - x1)
-end
-
-
--- returns direction from point 1 to point 2
--- returns a unit vector
-function getDir(x1, y1, x2, y2)
-    local deltaX, deltaY = delta(x1, y1, x2, y2)
-    return normalizeVect(deltaX, deltaY)
-end
-
-function translateDir(dx, dy, degrees, clockwise)
-	if clockwise == nil then
-		clockwise = true
-	end
-	
-	local radians = math.rad(degrees)
-	
-	if clockwise == false then
-		radians = -radians
-	end
-	
-	return (dx * math.cos(radians) - dy * math.sin(radians)) , (dx * math.sin(radians) + dy * math.cos(radians))
-end
-
--- Returns the angle in degrees between a pair of vectors
--- If only one vector is supplied, returns the angle between that and {x = 1, y = 0}
-function vectToDeg(dx1, dy1, dx2, dy2)
-	if dx2 == nil then
-		dx2, dy2 = getDir(0, 0, 1, 0)
-	else
-		dx2, dy2 = getDir(0, 0, dx2, dy2)
-	end
-	
-	dx1, dy1 = getDir(0, 0, dx1, dy1)
-	
-	local vect1Dist = distance(0, 0, dx1, dy1)
-	local vect2Dist = distance(0, 0, dx2, dy2)
-	local distBetween = distance(dx1, dy1, dx2, dy2)
-	
-	return math.deg(math.acos((vect1Dist * vect1Dist + vect2Dist * vect2Dist - distBetween * distBetween) / (2 * vect1Dist * vect2Dist)))
-end
-
--- makes any vector into a unit vector
-function normalizeVect(dx, dy)
-    dist = distance(0, 0, dx, dy)
-    return dx / dist, dy / dist
-end
-
--- returns vector of the given coordinates
--- might need to adjust how this works to handle directionality
--- first coordinate pair is the destination (direction to go towards)
--- second pair is the source (starting point)
-function delta(x1, y1, x2, y2)
-    return x1 - x2, y1 - y2
-end
-
--- returns the distance between two points
-function distance(x1, y1, x2, y2)
-    local deltaX, deltaY = delta(x1, y1, x2, y2) 
-    return math.sqrt(deltaX * deltaX + deltaY * deltaY)
-end 
-
--- takes a deltax/y vector as an argument as well as a minumum speed and maximum speed
--- and returns a vector in the same direction with the speed constrained between the given
--- speeds
-function normalizeSpeed(deltaX, deltaY, max, min)
-    local currentSpeed = distance(0, 0, deltaX, deltaY)
-    if currentSpeed < min then
-        return setSpeed(deltaX, deltaY, min)
-    elseif currentSpeed > max then 
-        return setSpeed(deltaX, deltaY, max)
-    else
-        return deltaX, deltaY
-    end
-end
-
--- sets the speed of the inputted vector to the inputted speed then
--- returns the new vector at that speed
-function setSpeed(deltaX, deltaY, speed)
-    dXNorm, dYNorm = normalizeVect(deltaX, deltaY)
-    return speed * dXNorm, speed * dYNorm
-end
-
-function getSpeed(deltX, deltY)
-	return distance(0, 0, deltX, deltY)
-end
-
-function getGravForceArcade(ballCircle, bodyCircle)
-
 end
 
 -- moved here to clean up the update function
@@ -332,44 +215,6 @@ function handleTouchInput()
 	end
 
 end
-
-
-function love.mousepressed(_x, _y)
-	_x, _y = rs.to_game(_x, _y)
-	devMenu:handleClicks(_x, _y)
-end
-
---[[
-dev menu functions
-This is a hacky solution but the dev menu doesnt need to follow best practices.
-Additionally this allows me to have finer control over the dev menu without creating
-an esoteric abstraction that only makes maintaining the codebasw harder.]]
-
-function getBallSpeed()
-	return getSpeed(ball.dx, ball.dy)
-end
-
-function setBallSpeed(newSpd)
-	ball.dx, ball.dy = setSpeed(ball.dx, ball.dy, newSpd) 
-end
-
-function getBallGoalSpd()
-	return ball.goalSpeed
-end
-
-function setBallGoalSpd(newSpd)
-	ball.goalSpeed = newSpd
-end
-
--- for using constants for gravity calcs
-function getGravCoef()
-	return constForce
-end
-
-function setGravCoef(newCoef)
-	constForce = newCoef
-end
-
 
 -- alternative algorithm for calculating ball gravity physics
 --[[ 1. the instant the ball collides with a field hit box, we take a snapshot of the following
